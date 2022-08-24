cd7a6e55c5f88fddf5e190f10f7d7d8dbf0d3e99
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2022-08-25 00:09:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // random number for dice 1
 var randomNumber1 = Math.floor( Math.random() * 6 ) + 1;
 
-var imgSrc1 = "images/dice" + randomNumber1 + ".png";
+var imgSrc1 = "dice" + randomNumber1 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", imgSrc1);
 
@@ -9,7 +9,7 @@ document.querySelectorAll("img")[0].setAttribute("src", imgSrc1);
 // random number for dice 2
 var randomNumber2 =  Math.floor( Math.random() * 6 ) + 1;
 
-var imgSrc2 = "images/dice" + randomNumber2 + ".png";
+var imgSrc2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", imgSrc2);
 
