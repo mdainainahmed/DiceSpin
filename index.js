f3ad44862fdcdb229277083d7909e72906505e4f
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2022-08-25 00:08:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// random number for dice 1
+var randomNumber1 = Math.floor( Math.random() * 6 ) + 1;
+
+var imgSrc1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", imgSrc1);
+
+
+// random number for dice 2
+var randomNumber2 =  Math.floor( Math.random() * 6 ) + 1;
+
+var imgSrc2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", imgSrc2);
+
+
+// check win
+if( randomNumber1 > randomNumber2 )
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+else if( randomNumber2 > randomNumber1 )
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+else
+    document.querySelector("h1").textContent = "Draw!";
